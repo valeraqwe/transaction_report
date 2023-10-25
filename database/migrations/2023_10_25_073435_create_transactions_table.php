b495534beb25b5582866cab22ff7531537dd1359
valeraqwe <valera.komar.1999@gmail.com>
2023-10-25 10:39:38 +0300

    Set up database and migrations.

---
+++ database/migrations/2023_10_25_073435_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number');
+            $table->date('date');
+            $table->time('time');
+            $table->text('program_list');
+            $table->decimal('duration', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
